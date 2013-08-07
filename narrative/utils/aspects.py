ffc79bc2499039b7953e22f2e1c2614222911636
NiM198939 <nirmal198939@gmail.com>
2013-08-07 05:27:47 -0500

    GA-315 This is fixed by adding No-login-session and the context for user-id in the entry for narration.

--- narrative/utils/aspects.py
+++ narrative/utils/aspects.py
@@ -10,12 +10,15 @@ class Aspects(object):
     '''
     # User Profile
     USER_IP = 'User-IP'
+    NO_LOGIN_SESSION='No-Login-Session'
 
     
     @classmethod
-    def generator_aspects(cls, context):
+    def generator_aspects(cls, context,no_login_session=None):
         aspect = {}
         aspect[cls.USER_IP] = context.get('x-forwarded-for', None)
+        if no_login_session:
+            aspect[cls.NO_LOGIN_SESSION] = no_login_session
         return aspect
         
         
