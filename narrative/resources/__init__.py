1e9e956fbe1e58093801016dc81ca557ae8bc94d
nirmal <nirmal198939@gmail.com>
2015-02-12 08:06:38 +0530

    Task 6119  * Added new resource Notification

--- narrative/resources/__init__.py
+++ narrative/resources/__init__.py
@@ -1,1 +1,2 @@
 from .entry import *
+from .notification import *
---
+++ narrative/resources/notification.py
@@ -0,0 +1,53 @@
+'''
+Created on Feb 3, 2015
+
+@author: nirmal
+'''
+
+from mesh.standard import *
+from scheme import *
+
+__all__ = ('Notification',)
+
+class Notification(Resource):
+    """A resource Notification."""
+
+    name = 'notification'
+    version = 1
+    requests = 'create put query'
+
+ 
+    class schema:
+        id = UUID(operators='equal', oncreate=True)
+        status = Enumeration('read unread', nullable=True, default='unread')
+        resource = Surrogate(nullable=False)
+        ownerID = UUID(operators='equal')
+        type = Token(nullable=False)
+        entity = Token(nullable=False)
+        created = DateTime(utc=True, sortable=True)
+        
+    
+    class markallread:
+        endpoint = ('MARKALLREAD', 'notification')
+        title = 'Update multiple notifications from unread to read '
+        schema = {
+            'notification_ids': Sequence(UUID(nonnull=True, description='set of uuid of notification.')),
+        }
+        responses = {
+            OK: Response(),
+            INVALID: Response(Errors),
+        }
+        
+    class task:
+        endpoint = ('TASK', 'notification')
+        title = 'Initiating a notification task'
+        schema = Structure(
+            structure={
+                'expire-notifications': {},
+            },
+            polymorphic_on='task',
+            nonempty=True)
+        responses = {
+            OK: Response(),
+            INVALID: Response(Errors),
+        }
